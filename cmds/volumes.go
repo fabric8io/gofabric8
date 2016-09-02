af54e0f0eced03892825879f33644c3ad6528957
James Strachan <james.strachan@gmail.com>
2016-09-02 12:51:07 +0100

    lets chmod PersistentVolumes (and use a valid capacity) when using mini* so that permissions work

--- cmds/volumes.go
+++ cmds/volumes.go
@@ -16,6 +16,13 @@
 package cmds
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
 	"github.com/fabric8io/gofabric8/client"
 	"github.com/fabric8io/gofabric8/util"
 	"github.com/spf13/cobra"
@@ -23,6 +30,10 @@ import (
 	"k8s.io/kubernetes/pkg/api/resource"
 	k8sclient "k8s.io/kubernetes/pkg/client/unversioned"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+)
+
+const (
+	sshCommandFlag = "ssh-command"
 )
 
 func NewCmdVolumes(f *cmdutil.Factory) *cobra.Command {
@@ -42,12 +53,12 @@ func NewCmdVolumes(f *cmdutil.Factory) *cobra.Command {
 
 				found, pendingClaimNames := findPendingPVS(c, ns)
 				if found {
-					createPV(c, ns, pendingClaimNames)
+					createPV(c, ns, pendingClaimNames, cmd)
 				}
 			}
 		},
 	}
-	//cmd.PersistentFlags().StringP(hostPathFlag, "", "", "Defines the host folder on which to define a persisent volume for single node setups")
+	cmd.PersistentFlags().String(sshCommandFlag, "", "the ssh command to run commands inside the VM of the single node cluster")
 	return cmd
 }
 
@@ -63,7 +74,7 @@ func findPendingPVS(c *k8sclient.Client, ns string) (bool, []string) {
 		pendingClaimNames := make([]string, 0, len(items))
 		for _, item := range items {
 			status := item.Status.Phase
-			if status == "Pending" {
+			if status == "Pending" || status == "Lost" {
 				pendingClaimNames = append(pendingClaimNames, item.ObjectMeta.Name)
 			}
 		}
@@ -74,14 +85,14 @@ func findPendingPVS(c *k8sclient.Client, ns string) (bool, []string) {
 	return false, nil
 }
 
-func createPV(c *k8sclient.Client, ns string, pvcNames []string) (Result, error) {
+func createPV(c *k8sclient.Client, ns string, pvcNames []string, cmd *cobra.Command) (Result, error) {
 
 	for _, pvcName := range pvcNames {
-		hostPath := "/" + pvcName
+		hostPath := "/data/" + pvcName
 		pvs := c.PersistentVolumes()
 		rc, err := pvs.List(api.ListOptions{})
 		if err != nil {
-			util.Errorf("Failed to load PersistentVolumes with error %v", err)
+			util.Errorf("Failed to load PersistentVolumes with error %v\n", err)
 		}
 		items := rc.Items
 		for _, volume := range items {
@@ -89,6 +100,11 @@ func createPV(c *k8sclient.Client, ns string, pvcNames []string) (Result, error)
 			if vname == pvcName {
 				util.Infof("Already created PersistentVolumes for %s\n", pvcName)
 			}
+		}
+
+		err = configureHostPathVolume(c, ns, hostPath, cmd)
+		if err != nil {
+			util.Errorf("Failed to configure the host path %s with error %v\n", hostPath, err)
 		}
 
 		// lets create a new PV
@@ -99,9 +115,9 @@ func createPV(c *k8sclient.Client, ns string, pvcNames []string) (Result, error)
 			},
 			Spec: api.PersistentVolumeSpec{
 				Capacity: api.ResourceList{
-					api.ResourceName(api.ResourceStorage): resource.MustParse("1G"),
+					api.ResourceName(api.ResourceStorage): resource.MustParse("5Gi"),
 				},
-				AccessModes: []api.PersistentVolumeAccessMode{api.ReadWriteMany},
+				AccessModes: []api.PersistentVolumeAccessMode{api.ReadWriteOnce},
 				PersistentVolumeSource: api.PersistentVolumeSource{
 					HostPath: &api.HostPathVolumeSource{Path: hostPath},
 				},
@@ -110,9 +126,75 @@ func createPV(c *k8sclient.Client, ns string, pvcNames []string) (Result, error)
 
 		_, err = pvs.Create(&pv)
 		if err != nil {
-			util.Errorf("Failed to create PersistentVolume %s at %s with error %v", pvcName, hostPath, err)
+			util.Errorf("Failed to create PersistentVolume %s at %s with error %v\n", pvcName, hostPath, err)
 		}
 	}
 
 	return Success, nil
+}
+
+// if we are on minikube or minishift lets try to create the
+// hostPath folders with relaxed persmissions
+func configureHostPathVolume(c *k8sclient.Client, ns string, hostPath string, corbaCmd *cobra.Command) error {
+	cli := corbaCmd.Flags().Lookup(sshCommandFlag).Value.String()
+
+	args := []string{"ssh"}
+	if len(cli) == 0 {
+		nodes, err := c.Nodes().List(api.ListOptions{})
+		if err != nil {
+			return err
+		}
+		if len(nodes.Items) == 1 {
+			node := nodes.Items[0]
+			if node.Name == minikubeNodeName || node.Name == minishiftNodeName {
+				// lets figure out which one we are
+				// TODO there's no obvious annotation yet to know
+				// if it was created via minikube or minishift
+				// so lets look at the images
+				cli = "minikube"
+
+				for _, image := range node.Status.Images {
+					for _, imageName := range image.Names {
+						if strings.HasPrefix(imageName, "openshift/origin-pod:") {
+							cli = "minishift"
+							break
+						}
+					}
+
+				}
+			}
+		}
+	}
+	if len(cli) == 0 {
+		// lets default to using vagrant if we have a Vagrantfile
+		if _, err := os.Stat("Vagrantfile"); os.IsNotExist(err) {
+			cli = "vagrant"
+		}
+	}
+	if len(cli) == 0 {
+		return nil
+	}
+
+	util.Infof("About to modify host paths on the VM via the command: %s %s\n", cli, strings.Join(args, " "))
+
+	cmd := exec.Command(cli, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	shellInput := fmt.Sprintf("echo ensuring the hostPath is created %s\nsudo mkdir -p %s\nsudo chmod 777 %s\n", hostPath, hostPath, hostPath)
+
+	cmd.Stdin = bytes.NewBufferString(shellInput)
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		printErr(err)
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printStatus(waitStatus.ExitStatus())
+		}
+		return err
+	} else {
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printStatus(waitStatus.ExitStatus())
+		return nil
+	}
+
 }
