f87152af5afe16e097c2f3c516307ed57d827c1a
Chmouel Boudjnah <chmouel@chmouel.com>
2017-06-29 14:13:31 +0200

    Add zsh completion
    It seems to be actually straightforward using the code from kube

--- cmds/completion.go
+++ cmds/completion.go
@@ -16,6 +16,7 @@
 package cmds
 
 import (
+	"bytes"
 	"io"
 
 	"github.com/spf13/cobra"
@@ -37,12 +38,18 @@ framework. It must be sourced before sourcing the gofabric8 completion, i.e. on 
 $ brew install bash-completion
 $ source $(brew --prefix)/etc/bash_completion
 $ source <(gofabric8 completion bash)
-`
+
+If you use zsh*, the following will load gofabric8 zsh completion:
+
+$ source <(gofabric8 completion zsh)
+
+* zsh completions are only supported in versions of zsh >= 5.2`
 )
 
 var (
 	completion_shells = map[string]func(out io.Writer, cmd *cobra.Command) error{
 		"bash": runCompletionBash,
+		"zsh":  runCompletionZsh,
 	}
 )
 
@@ -54,7 +61,7 @@ func NewCmdCompletion(f *cmdutil.Factory, out io.Writer) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:     "completion SHELL",
-		Short:   "Output shell completion code for the given shell (bash)",
+		Short:   "Output shell completion code for the given shell (bash or zsh)",
 		Long:    completion_long,
 		Example: completion_example,
 		Run: func(cmd *cobra.Command, args []string) {
@@ -84,4 +91,170 @@ func RunCompletion(f *cmdutil.Factory, out io.Writer, cmd *cobra.Command, args [
 
 func runCompletionBash(out io.Writer, kubectl *cobra.Command) error {
 	return kubectl.GenBashCompletion(out)
+}
+
+func runCompletionZsh(out io.Writer, kubeadm *cobra.Command) error {
+	zsh_initialization := `
+__kubeadm_bash_source() {
+	alias shopt=':'
+	alias _expand=_bash_expand
+	alias _complete=_bash_comp
+	emulate -L sh
+	setopt kshglob noshglob braceexpand
+
+	source "$@"
+}
+
+__kubeadm_type() {
+	# -t is not supported by zsh
+	if [ "$1" == "-t" ]; then
+		shift
+
+		# fake Bash 4 to disable "complete -o nospace". Instead
+		# "compopt +-o nospace" is used in the code to toggle trailing
+		# spaces. We don't support that, but leave trailing spaces on
+		# all the time
+		if [ "$1" = "__kubeadm_compopt" ]; then
+			echo builtin
+			return 0
+		fi
+	fi
+	type "$@"
+}
+
+__kubeadm_compgen() {
+	local completions w
+	completions=( $(compgen "$@") ) || return $?
+
+	# filter by given word as prefix
+	while [[ "$1" = -* && "$1" != -- ]]; do
+		shift
+		shift
+	done
+	if [[ "$1" == -- ]]; then
+		shift
+	fi
+	for w in "${completions[@]}"; do
+		if [[ "${w}" = "$1"* ]]; then
+			echo "${w}"
+		fi
+	done
+}
+
+__kubeadm_compopt() {
+	true # don't do anything. Not supported by bashcompinit in zsh
+}
+
+__kubeadm_declare() {
+	if [ "$1" == "-F" ]; then
+		whence -w "$@"
+	else
+		builtin declare "$@"
+	fi
+}
+
+__kubeadm_ltrim_colon_completions()
+{
+	if [[ "$1" == *:* && "$COMP_WORDBREAKS" == *:* ]]; then
+		# Remove colon-word prefix from COMPREPLY items
+		local colon_word=${1%${1##*:}}
+		local i=${#COMPREPLY[*]}
+		while [[ $((--i)) -ge 0 ]]; do
+			COMPREPLY[$i]=${COMPREPLY[$i]#"$colon_word"}
+		done
+	fi
+}
+
+__kubeadm_get_comp_words_by_ref() {
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[${COMP_CWORD}-1]}"
+	words=("${COMP_WORDS[@]}")
+	cword=("${COMP_CWORD[@]}")
+}
+
+__kubeadm_filedir() {
+	local RET OLD_IFS w qw
+
+	__debug "_filedir $@ cur=$cur"
+	if [[ "$1" = \~* ]]; then
+		# somehow does not work. Maybe, zsh does not call this at all
+		eval echo "$1"
+		return 0
+	fi
+
+	OLD_IFS="$IFS"
+	IFS=$'\n'
+	if [ "$1" = "-d" ]; then
+		shift
+		RET=( $(compgen -d) )
+	else
+		RET=( $(compgen -f) )
+	fi
+	IFS="$OLD_IFS"
+
+	IFS="," __debug "RET=${RET[@]} len=${#RET[@]}"
+
+	for w in ${RET[@]}; do
+		if [[ ! "${w}" = "${cur}"* ]]; then
+			continue
+		fi
+		if eval "[[ \"\${w}\" = *.$1 || -d \"\${w}\" ]]"; then
+			qw="$(__kubeadm_quote "${w}")"
+			if [ -d "${w}" ]; then
+				COMPREPLY+=("${qw}/")
+			else
+				COMPREPLY+=("${qw}")
+			fi
+		fi
+	done
+}
+
+__kubeadm_quote() {
+    if [[ $1 == \'* || $1 == \"* ]]; then
+        # Leave out first character
+        printf %q "${1:1}"
+    else
+    	printf %q "$1"
+    fi
+}
+
+autoload -U +X bashcompinit && bashcompinit
+
+# use word boundary patterns for BSD or GNU sed
+LWORD='[[:<:]]'
+RWORD='[[:>:]]'
+if sed --help 2>&1 | grep -q GNU; then
+	LWORD='\<'
+	RWORD='\>'
+fi
+
+__kubeadm_convert_bash_to_zsh() {
+	sed \
+	-e 's/declare -F/whence -w/' \
+	-e 's/local \([a-zA-Z0-9_]*\)=/local \1; \1=/' \
+	-e 's/flags+=("\(--.*\)=")/flags+=("\1"); two_word_flags+=("\1")/' \
+	-e 's/must_have_one_flag+=("\(--.*\)=")/must_have_one_flag+=("\1")/' \
+	-e "s/${LWORD}_filedir${RWORD}/__kubeadm_filedir/g" \
+	-e "s/${LWORD}_get_comp_words_by_ref${RWORD}/__kubeadm_get_comp_words_by_ref/g" \
+	-e "s/${LWORD}__ltrim_colon_completions${RWORD}/__kubeadm_ltrim_colon_completions/g" \
+	-e "s/${LWORD}compgen${RWORD}/__kubeadm_compgen/g" \
+	-e "s/${LWORD}compopt${RWORD}/__kubeadm_compopt/g" \
+	-e "s/${LWORD}declare${RWORD}/__kubeadm_declare/g" \
+	-e "s/\\\$(type${RWORD}/\$(__kubeadm_type/g" \
+	<<'BASH_COMPLETION_EOF'
+`
+	out.Write([]byte(zsh_initialization))
+
+	buf := new(bytes.Buffer)
+	kubeadm.GenBashCompletion(buf)
+	out.Write(buf.Bytes())
+
+	zsh_tail := `
+BASH_COMPLETION_EOF
+}
+
+__kubeadm_bash_source <(__kubeadm_convert_bash_to_zsh)
+`
+	out.Write([]byte(zsh_tail))
+	return nil
 }
